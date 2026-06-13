8f7daedc88a0d815ba7f6291cd33cf9bac92fa44
Andell <56235026+Andell4301@users.noreply.github.com>
2026-06-12 23:25:30 -0400

    use undetected for oauth token script

--- scripts/get_oauth_token.py
+++ scripts/get_oauth_token.py
@@ -1,24 +1,25 @@
 # ruff: noqa: T201
-
-from selenium import webdriver
+import undetected
+from selenium.webdriver.common.selenium_manager import SeleniumManager
 from selenium.webdriver.support.ui import WebDriverWait
+from undetected.patcher import Patcher
+
+
+def get_chrome_binary() -> str:
+    return SeleniumManager().binary_paths(["--browser", "chrome", "--browser-version", "stable"])["browser_path"]
 
 
 def get_oauth_token() -> str:
-    options = webdriver.ChromeOptions()
-    options.add_argument(
-        argument="--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"  # noqa: E501
-    )
-    options.add_argument(argument="--disable-blink-features=AutomationControlled")
-    options.add_experimental_option(name="excludeSwitches", value=["enable-automation"])
-    options.add_experimental_option(name="useAutomationExtension", value=False)
-    with webdriver.Chrome(options=options) as driver:
+    chrome_binary = get_chrome_binary()
+    Patcher.patch(browser_executable_path=chrome_binary)
+
+    with undetected.Chrome(browser_executable_path=chrome_binary, user_multi_procs=True) as driver:
         # Some other scripts use https://accounts.google.com/EmbeddedSetup
         # That will work here too, but for me, using OAuth from that page to
         # gen an AAS token gives a 4xx error, whereas this one works
         driver.get("https://accounts.google.com/embedded/setup/v2/android")
         WebDriverWait(driver, 300).until(lambda d: d.get_cookie("oauth_token"))
-        return driver.get_cookie("oauth_token")["value"]  # type: ignore[reportOptionalSubscript]
+        return driver.get_cookie("oauth_token")["value"]
 
 
 if __name__ == "__main__":
